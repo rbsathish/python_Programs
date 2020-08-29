913bdc7cca5b14a03c37b94f209b744573b5ff8c
sathish kumar <rbsathish27@gmail.com>
2020-08-29 12:17:47 +0530

    added new que and ans

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # python_Programs
- In this repo just solving Basic python programs and uploading for reference 
+In this repo just solving Basic python programs and uploading for reference. 
+
+
+Questions:
+
+    you can find the questions from Questions.txt
+
+
+Solutions:
+    
+    And answer from solutions.py
+
+    
--- solutions.py
+++ solutions.py
@@ -177,3 +177,41 @@ cric_score = [1,2,0,0,4,1,6,2,1,3]
 cri = Batsman_score_calculator()
 cri.cric_(cric_score)   
 
+####################### 5th program ######################################################
+
+#Counting String if tie means using lexicographically
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    counts_x = sorted(counts.items(), key=lambda kv: kv[1])
+    f = counts_x[-1]
+    s = counts_x[-2]
+    k,v = counts_x[-1]
+    k2,v2 = counts_x[-2]
+    l=[]
+    
+    if v == v2:
+        l.append(k)
+        l.append(k2)
+        l.sort()
+        print("\nOutput :")
+        print("\tTie in the given string so using lexicographically order")
+        print("\t",l)
+    else:
+        print("\nOutput :")
+        print("\tThe most in the given string")
+        print("\t",counts_x[-1])
+        
+########### get input from user ##################### 
+# inp = str(input("\nEnter the input string : " ))
+# word_count(inp)
+
+############ passing direct input ####################
+print(word_count("hey hi how are hey hell heat honey  hi hi hi"))
